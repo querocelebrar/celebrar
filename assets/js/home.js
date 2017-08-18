645665c27ef04dd2df95db4611210292c134e693
Joao Victor <xavier.joaovictor@gmail.com>
2017-08-18 18:13:04 -0300

    Fixing boxes

--- assets/js/home.js
+++ assets/js/home.js
@@ -26,3 +26,28 @@ $(window).scroll(function() {
 $('#headerButton #talkButton').click(function() {
   $.post('/converse-comigo');
 });
+
+$('#readMoreRifas').click(function () {
+  document.getElementById('DescriptionTitle').innerHTML = "RIFAS ELETRÔNICAS";
+  document.getElementById('Description').innerHTML = "<br>Nossa plataforma vai disponibilizar, mensalmente, rifas eletrônicas para que você possa vender e rentabilizar ainda mais sua \
+                                                          festa. Toda rifa vendida por seu login vai gerar uma boa comissão que vai direto para sua conta dentro da plataforma.";
+});
+
+$('#readMoreVaquinhas').click(function () {
+  document.getElementById('DescriptionTitle').innerHTML = "CROWDFUNDING / <br>  VAQUINHAS";
+  document.getElementById('Description').innerHTML = "<br>Dentro da plataforma você poderá definir objetivos e convidar\
+                                                          pessoas de sua lista para te ajudar a alcançar eles. Por exemplo: Você define um objetivo de\
+                                                          contratar um dupla sertaneja para sua festa que tem o custo de R$1.000,00 e seleciona\
+                                                          pessoas de sua lista de convidados para te ajudar no objetivo (Padrinhos, primos, amigos\
+                                                          próximos etc...). <br><br>Dentro daquele objetivo, as pessoas que você selecionou podem fazer\
+                                                          contribuições e as mesmas vão diretamente para sua conta dentro da plataforma.";
+});
+
+$('#readMoreVendas').click(function () {
+  document.getElementById('DescriptionTitle').innerHTML = "VENDA DE CONVITES";
+  document.getElementById('Description').innerHTML = "<br>Sempre acontece de você chamar algum convidado e ele ter algum\
+                                                          agregado para levar né? <br><br>Nesse caso, você vai poder gerar um link para venda do convite a esse\
+                                                          agregado e quando ele efetuar o pagamento o dinheiro vai direto para sua conta dentro da\
+                                                          plataforma.";
+});
+
